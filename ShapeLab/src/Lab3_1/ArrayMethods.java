0b9c90093f3f4e1bdbb95dc9ff43f7436f13059b
BT_1N3_26 <BT_1N3_26@BT_1N3_26.bktech.local>
2017-11-08 13:16:07 -0500

    partner push

--- ShapeLab/src/Lab3_1/ArrayMethods.java
+++ ShapeLab/src/Lab3_1/ArrayMethods.java
@@ -4,25 +4,33 @@ public class ArrayMethods {
 	
 	public static void main (String[] args)
 	{
-		int[] list = {1,2,3,1,2};
+		int[] list = {1,2,2,3,4,5};
 		
 		removeDuplicates(list);
 	}
 	
 	public static int[] removeDuplicates(int[] list)
 	{
-		int[] newArr = new int[3];
+		int[] newArr = new int[list.length];
 		for (int x = 0; x<list.length-1;x++)
+			//x =0, i = 1, 
 		{
 			for (int i = x+1; i<list.length; i++)
 			{
 				if (!(list[x]==list[i]))
 				{
 					newArr[x] = list[x];
+					//System.out.println(newArr);
 				}
 			}
 		}
+		
+		for ( int y = 0 ; y<newArr.length; y++)
+		{
+			System.out.println(newArr[y]);
+		}
 		return newArr;
+		
 	}
 
 }
