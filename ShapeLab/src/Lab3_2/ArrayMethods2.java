0a67bbefa154c31f61d646aee6b13f76841307d9
BT_1N3_26 <BT_1N3_26@BT_1N3_26.bktech.local>
2017-11-16 13:11:49 -0500

    3.2.0

---
+++ ShapeLab/src/Lab3_2/ArrayMethods2.java
@@ -0,0 +1,17 @@
+package Lab3_2;
+
+public class ArrayMethods2 {
+	
+	public static void main(String[] args)
+	{
+		String[] list1 = {"a", "b", "c", "e"};
+		String[] list2 = {"a", "d", "e"};
+	}
+	
+	public static String[] merge(String[] list1, String[] list2)
+	{
+		String[] mergedList = new String[list1.length+list2.length];
+		
+	}
+	
+}
