93f80aa55a453fe96adb4fcf88add1b0e5709f9c
danaravvin <danaravvin@danas-air.home>
2017-11-16 21:23:31 -0500

    3.2.1

--- ShapeLab/src/Lab3_2/ArrayMethods2.java
+++ ShapeLab/src/Lab3_2/ArrayMethods2.java
@@ -6,12 +6,29 @@ public class ArrayMethods2 {
 	{
 		String[] list1 = {"a", "b", "c", "e"};
 		String[] list2 = {"a", "d", "e"};
+		// merged list should print: a , a , b, c, d, e, e
+		
+		System.out.print(list1[0].compareTo(list2[1]));
 	}
 	
 	public static String[] merge(String[] list1, String[] list2)
 	{
 		String[] mergedList = new String[list1.length+list2.length];
 		
+		for (int i = 0; i<mergedList.length; i++)
+		{
+			if(list1[i].compareTo(list2[i]))
+			{
+				mergedList[i] = list1[i];
+				mergedList[i+1] = list1[i];
+				i++;
+			}
+			if(list1[i].compareTo(list2[i]) < 0)
+			{
+				//list1[i] is closer to a than list2[i]
+				
+			}
+		}
 	}
 	
 }
