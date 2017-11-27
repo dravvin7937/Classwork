81da0cccc63541ba092a5058c5e6dd17f0d1c1ef
BT_1N3_26 <BT_1N3_26@BT_1N3_26.bktech.local>
2017-11-27 13:14:53 -0500

    push

--- ShapeLab/src/Lab3_3/ArrayMethods3.java
+++ ShapeLab/src/Lab3_3/ArrayMethods3.java
@@ -5,12 +5,13 @@ public class ArrayMethods3 {
 	public static void main(String[] args)
 	{
 		int[] list = {1, 2, 4, 3, 2, 7, 1};
+		String[] list2 = {"a", "b", "c", "e", "d"};
 		
-		bubbleSort(list);
+		bubbleSort(list2);
 		
-		for (int num : list)
+		for (String letter : list2)
 		{
-			System.out.println(num);
+			System.out.println(letter);
 		}
 	}
 
@@ -22,26 +23,38 @@ public class ArrayMethods3 {
 		}
 	}
 	
-	public static void swap(int[] arr, int i, int j)
+	public static void swap(String[] arr, int i, int j)
 	{
-		int temp = arr[i];
+		String temp = arr[i];
 		arr[i] = arr[j];
 		arr[j] = temp;
 	}
-	public static void bubbleSort(int[] list1)
+	
+	//make into string 
+	public static void bubbleSort(String[] list1)
 	{
 		for(int i = 0; i<list1.length-1; i++) 
 		{
-			if(list1[i+1] < list1[i])
+			if(list1[i+1].compareTo(list1[i]) <= 0)
 			{
 				swap(list1,i,i+1);
 				bubbleSort(list1);
 			}
 		}
-		
 	}
+	
 	public static void selectionSort( double [] list1)
 	{
+		int start = 0;
+		double lowest = 0;
+		while(start<list1.length) 
+		{
+			lowest = list1[start];
+			if(list1[start+1] < list1[start])
+			{
+				
+			}
+		}
 		
 	}
 	
