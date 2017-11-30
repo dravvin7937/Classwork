60c1b2dece33a88cab02e40c13633641b42081e3
BT_1N3_26 <BT_1N3_26@BT_1N3_26.bktech.local>
2017-11-30 13:06:44 -0500

    update

--- ShapeLab/src/Lab3_4/ArrayMethods4.java
+++ ShapeLab/src/Lab3_4/ArrayMethods4.java
@@ -10,7 +10,13 @@ public class ArrayMethods4 {
 	public static int partition(int[] list1, int front, int back)
 	{
 		//returns position of the pivot 
+		int pivot = list1[front];
+		int pivPosition = front;
 		
+		while(back > front)
+		{
+			
+		}
 	}
 	public static void quickSort(int[] list1, int front, int back)
 	{
