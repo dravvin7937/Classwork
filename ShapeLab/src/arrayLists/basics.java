4986bc0be7222c0ec7358931c3cf0169d6a65484
BT_1N3_26 <BT_1N3_26@BT_1N3_26.bktech.local>
2017-12-13 13:09:38 -0500

    starting lists

---
+++ ShapeLab/src/arrayLists/basics.java
@@ -0,0 +1,27 @@
+package arrayLists;
+
+import java.util.List;
+
+public class basics {
+	
+	public static void main(String[] args)
+	{
+		List<Integer> list = new List<Integer>;
+	}
+	
+	public static List<Integer> removeDupes(List<Integer> nums)
+	{
+		for (int i = 0; i < nums.size()-1; i++)
+		{
+			for (int x = 0; x < nums.size(); x++)
+			{
+				if (nums.get(i) == nums.get(x))
+				{
+					nums.remove(x);
+				}
+			}
+		}
+		return nums;
+	}
+	
+}
