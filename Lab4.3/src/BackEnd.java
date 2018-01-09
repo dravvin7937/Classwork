25f2504cbdcc0186a92a42e341c7e0b641027373
danaravvin <danaravvin@kindle-a7ed31c1b.home>
2018-01-09 00:03:22 -0500

    timer implemented, just need to show everything on the screen

--- Lab4.3/src/BackEnd.java
+++ Lab4.3/src/BackEnd.java
@@ -1,4 +1,25 @@
+import javafx.animation.AnimationTimer;
 
 public class BackEnd {
 
+	
+	static AnimationTimer startTime = new AnimationTimer()
+	{
+		public void handle(long now)
+		{
+			if(now > GameCode.timer)
+			{
+				GameCode.timer = now + 10000000000L;
+				GameCode.game = !GameCode.game;
+			}
+			if(!GameCode.game)
+			{
+				GameCode.button.setDisable(true);
+				GameCode.status.setText("Game Over!");
+			}
+			
+			GameCode.playerScore.setText("Score: " + Integer.toString(GameCode.score) + "       ");
+		}
+	};
+	
 }
--- Lab4.3/src/GameCode.java
+++ Lab4.3/src/GameCode.java
@@ -1,44 +1,93 @@
+import java.util.Timer;
+
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-public class GameCode extends Application{
+//public class GameCode extends Application{
+//
+//	/*
+//	 * create a button in the center of the screen
+//	 * create a counter
+//	 * check if the button is clicked
+//	 * 	if it is clicked, add to the counter
+//	 * 
+//	 * create timer that goes up to 10 seconds and begins upon the first click
+//	 * when timer ends record player's name and high score (number of clicks from the counter) and add to the csv
+//	 * 
+//	 * display score list
+//	 */
+//	int score = 0;
+//	boolean game = true;
+//	public static void main(String[] args) 
+//	{
+//		
+//		
+//		Application.launch(args);
+//	}
+//	
+//	@Override
+//	public void start(Stage stage)
+//	{
+//		//sets up the stage and button
+//		stage.setTitle("FastClicker Game");
+//		Button button = new Button();
+//		button.setText("Click me as fast as you can");
+//		
+////		primaryStage.setScene(new Scene(root, 300, 250));
+////		primaryStage.show();
+//		
+//		//creates the score increase for every click
+//		button.setOnAction(new EventHandler() {
+//		    @Override
+//		    public void handle(ActionEvent actionEvent) {
+//		        if (game == true)
+//		        {
+//		        		score++;
+//		        }
+//		    }
+//		});
+//		
+//		Scene scene = new Scene(button, 200, 100);
+//		stage.setScene(scene);
+//        stage.show();
+//	}
+//	
 
-	/*
-	 * create a button in the center of the screen
-	 * create a counter
-	 * check if the button is clicked
-	 * 	if it is clicked, add to the counter
-	 * 
-	 * create timer that goes up to 10 seconds and begins upon the first click
-	 * when timer ends record player's name and high score (number of clicks from the counter) and add to the csv
-	 * 
-	 * display score list
-	 */
+public class GameCode extends Application  {
 	
+	static int score = 0;
+	static boolean game = true;
+	static Text playerScore = new Text("Score: ");
+	static Text status = new Text("Game On!");
+    static Button button = new Button("Click me!");
+    static long timer = System.nanoTime() + 10000000000L;
+    
 	public static void main(String[] args) 
 	{
-		int score = 0;
-		boolean game = true;
-		
-		Application.launch(args);
-	}
+        Application.launch(args);
+    }
 	
-	@Override
-	public void start(Stage stage)
-	{
-		//sets up the stage and button
-		stage.setTitle("FastClicker Game");
-		Button button = new Button();
-		button.setText("Click me as fast as you can");
-		
-//		primaryStage.setScene(new Scene(root, 300, 250));
-//		primaryStage.show();
-		
-		//creates the score increase for every click
-		button.setOnAction(new EventHandler() {
+    @Override
+    public void start(Stage primaryStage)
+    {
+        primaryStage.setTitle("FastClicker Game");
+
+
+        Scene scene = new Scene(button, 200, 100);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+       
+        
+      //creates the score increase for every click
+		button.setOnAction(new EventHandler<ActionEvent>() {
 		    @Override
 		    public void handle(ActionEvent actionEvent) {
 		        if (game == true)
@@ -47,10 +96,12 @@ public class GameCode extends Application{
 		        }
 		    }
 		});
+        
 		
-		Scene scene = new Scene(button, 200, 100);
-        primaryStage.setScene(scene);
-        primaryStage.show();
-	}
-	
+		BackEnd.startTime.start();
+    }
+
+    
 }
+
+
